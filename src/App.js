69100c3eeba07c6770081ed0651de3eda8236e75
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-07-03 01:51:03 +0200

    Add display of current weather and icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
 const App = () => {
+  const [weather, setWeather] = useState(null);
+
+
+  const fetchWeather = () => {
+    const API_KEY = process.env.REACT_APP_MY_API_ID;
+    const city = "Prague"; 
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+      })
+      .catch((error) => {
+        console.log("Chyba při načítání dat o počasí:", error);
+      });
+  };
+
+  const formatTime = (unixTimestamp) => {
+    const date = new Date(unixTimestamp * 1000);
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    return `${hours}:${minutes}`;
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+
   return (
     <div className="App">
       <div className="container">
@@ -13,41 +42,42 @@ const App = () => {
             <button className="button">City03</button>
           </div> */}
           <div className="weather__current">
+          {weather ? (
+              <>
             <h2 className="weather__city" id="mesto">
-              City, Country
+            {weather.name}
             </h2>
             <div className="weather__inner weather__inner--center">
               <div className="weather__section weather__section--temp">
                 <span className="weather__temp-value" id="teplota">
-                  --
+                {Math.round(weather.main.temp)}
                 </span>
                 <span className="weather__temp-unit">°C</span>
                 <div className="weather__description" id="popis">
-                  --
+                {weather.weather[0].description}
                 </div>
               </div>
               <div
                 className="weather__section weather__section--icon"
                 id="ikona"
               >
-                --
-                {/* <img
-                  src={URL FROM OPEN WEATHER}
-                  alt="current weather icon"
-                /> */}
+                <img
+                      src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                      alt="current weather icon"
+                /> 
               </div>
             </div>
             <div className="weather__inner">
               <div className="weather__section">
                 <h3 className="weather__title">Wind</h3>
                 <div className="weather__value">
-                  <span id="wind">--</span> km/h
+                <span>{weather.wind.speed}</span> km/h
                 </div>
               </div>
               <div className="weather__section">
                 <h3 className="weather__title">Humidity</h3>
                 <div className="weather__value">
-                  <span id="humidity">--</span> %
+                <span>{weather.main.humidity}</span> %
                 </div>
               </div>
             </div>
@@ -55,16 +85,24 @@ const App = () => {
               <div className="weather__section">
                 <h3 className="weather__title">Sunrise</h3>
                 <div className="weather__value">
-                  <span id="sunrise">--</span>
+                <span>
+                {formatTime(weather.sys.sunrise)}
+                </span>
                 </div>
               </div>
               <div className="weather__section">
                 <h3 className="weather__title">Sunset</h3>
                 <div className="weather__value">
-                  <span id="sunset">--</span>
+                <span>
+                {formatTime(weather.sys.sunset)}
+                </span>
                 </div>
               </div>
             </div>
+            </>
+) : (
+  <p>Loading...</p>
+)}
           </div>
           <div class="weather__forecast" id="predpoved">
             <div class="forecast">
