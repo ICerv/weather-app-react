d370d606e9f0a15c1d978f2e396a7a2663a74ed4
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-07-03 03:12:49 +0200

    Add WeatherCurrent component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
+import { WeatherCurrent } from "./components/WeatherCurrent";
 
 const App = () => {
   const [weather, setWeather] = useState(null);
@@ -15,16 +16,10 @@ const App = () => {
         setWeather(data);
       })
       .catch((error) => {
-        console.log("Chyba při načítání dat o počasí:", error);
+        console.log("Error loading weather data:", error);
       });
   };
 
-  const formatTime = (unixTimestamp) => {
-    const date = new Date(unixTimestamp * 1000);
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    return `${hours}:${minutes}`;
-  };
 
   useEffect(() => {
     fetchWeather();
@@ -36,74 +31,14 @@ const App = () => {
       <div className="container">
         <h1>My Weather App</h1>
         <div className="weather">
+        <WeatherCurrent weather={weather} />
+
           {/* <div className="button-group">
             <button className="button">City01</button>
             <button className="button">City02</button>
             <button className="button">City03</button>
           </div> */}
-          <div className="weather__current">
-          {weather ? (
-              <>
-            <h2 className="weather__city" id="mesto">
-            {weather.name}
-            </h2>
-            <div className="weather__inner weather__inner--center">
-              <div className="weather__section weather__section--temp">
-                <span className="weather__temp-value" id="teplota">
-                {Math.round(weather.main.temp)}
-                </span>
-                <span className="weather__temp-unit">°C</span>
-                <div className="weather__description" id="popis">
-                {weather.weather[0].description}
-                </div>
-              </div>
-              <div
-                className="weather__section weather__section--icon"
-                id="ikona"
-              >
-                <img
-                      src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                      alt="current weather icon"
-                /> 
-              </div>
-            </div>
-            <div className="weather__inner">
-              <div className="weather__section">
-                <h3 className="weather__title">Wind</h3>
-                <div className="weather__value">
-                <span>{weather.wind.speed}</span> km/h
-                </div>
-              </div>
-              <div className="weather__section">
-                <h3 className="weather__title">Humidity</h3>
-                <div className="weather__value">
-                <span>{weather.main.humidity}</span> %
-                </div>
-              </div>
-            </div>
-            <div className="weather__inner">
-              <div className="weather__section">
-                <h3 className="weather__title">Sunrise</h3>
-                <div className="weather__value">
-                <span>
-                {formatTime(weather.sys.sunrise)}
-                </span>
-                </div>
-              </div>
-              <div className="weather__section">
-                <h3 className="weather__title">Sunset</h3>
-                <div className="weather__value">
-                <span>
-                {formatTime(weather.sys.sunset)}
-                </span>
-                </div>
-              </div>
-            </div>
-            </>
-) : (
-  <p>Loading...</p>
-)}
-          </div>
+         
           <div class="weather__forecast" id="predpoved">
             <div class="forecast">
               <div class="forecast__day">Day, date</div>
---
+++ src/components/WeatherCurrent/index.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './style.css';
+
+export const WeatherCurrent = ({ weather }) => {
+  const formatTime = (unixTimestamp) => {
+    const date = new Date(unixTimestamp * 1000);
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${hours}:${minutes}`;
+  };
+
+  if (!weather) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div className="weather__current">
+      {weather ? (
+        <>
+          <h2 className="weather__city" id="mesto">
+            {weather.name}
+          </h2>
+          <div className="weather__inner weather__inner--center">
+            <div className="weather__section weather__section--temp">
+              <span className="weather__temp-value" id="teplota">
+                {Math.round(weather.main.temp)}
+              </span>
+              <span className="weather__temp-unit">°C</span>
+              <div className="weather__description" id="popis">
+                {weather.weather[0].description}
+              </div>
+            </div>
+            <div className="weather__section weather__section--icon" id="ikona">
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt="current weather icon"
+              />
+            </div>
+          </div>
+          <div className="weather__inner">
+            <div className="weather__section">
+              <h3 className="weather__title">Wind</h3>
+              <div className="weather__value">
+                <span>{weather.wind.speed}</span> km/h
+              </div>
+            </div>
+            <div className="weather__section">
+              <h3 className="weather__title">Humidity</h3>
+              <div className="weather__value">
+                <span>{weather.main.humidity}</span> %
+              </div>
+            </div>
+          </div>
+          <div className="weather__inner">
+            <div className="weather__section">
+              <h3 className="weather__title">Sunrise</h3>
+              <div className="weather__value">
+                <span>{formatTime(weather.sys.sunrise)}</span>
+              </div>
+            </div>
+            <div className="weather__section">
+              <h3 className="weather__title">Sunset</h3>
+              <div className="weather__value">
+                <span>{formatTime(weather.sys.sunset)}</span>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
